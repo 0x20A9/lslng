b55888be8795b55c4adad3f489d5fba8dfa0b3d8
0x20A9 <Gai0cohng9xuo@proton.me>
2022-08-11 16:19:42 +0900

    horziontal format

--- main.sh
+++ main.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-# shebang
-
-
 # --------------------------------------------------------------------
 # version info search regex aided by https://stackoverflow.com/a/19854164
 # --------------------------------------------------------------------
 
 
+printtitle() {
 
-pprint() {
 	Color_Off='\033[0m'       # Text Reset
 	BYellow='\033[1;33m'      # Yellow
 	BWhite='\033[1;37m'       # White
 	BGreen='\033[1;32m'       # Green
 	
+	tC="$BWhite"
+	printf " $tC$1$Color_Off - |"
+	return
+}
+
+pprint() {
+	Color_Off='\033[0m'       # Text Reset
 	Red='\033[0;31m'          # Red
 	Green='\033[0;32m'        # Green
 	Yellow='\033[0;33m'       # Yellow
@@ -23,14 +27,13 @@ pprint() {
 	Cyan='\033[0;36m'         # Cyan
 	White='\033[0;37m'        # White
 
-	langC="$BWhite"
+	langC="$White"
 	majorC="$Green"
 	minor1C="$Cyan"
 	minor2C="$Purple"
 	
-	separator=" $White─$Color_Off "
 	
-	printf "$langC$1$Color_Off$separator"
+	printf " $langC$1$Color_Off "
 
 	export IFS="."
 	count=0
@@ -56,8 +59,8 @@ pprint() {
 	if [ $count = 2 ]; then
 		printf "$White.$Color_Off"$minor2C"0"$Color_Off""
 	fi
-
-	printf "\n"
+	
+	printf " |"
 
 	return
 
@@ -73,12 +76,15 @@ get_python() {
 	for item in "python" "python3" "python2"; do
 		if command -v $item >/dev/null 2>&1; then
 			version=$( $item -c 'import sys; print(".".join([str(v) for v in sys.version_info[:3]]))' )
+			if [ $count = 0 ]; then
+				printtitle "python"
+			fi
+
 			pprint "$item" "$version"
 			count=$((count + 1))
 		fi
 	done
-
-
+	
 	if (( $count == 0 )); then
 		return
 	fi
@@ -93,6 +99,8 @@ get_lua() {
 	fi
 	
 	version=$(lua -v | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)')
+	
+	printtitle "lua"
 	pprint "lua" "$version"
 	
 	for item in "luac" "luajit"; do
@@ -110,7 +118,8 @@ get_perl() {
 	if ! command -v perl >/dev/null 2>&1; then
 		return
 	fi
-
+	
+	printtitle "perl"
 	{ version=$(perl -e 'print substr($^V, 1)'); } 2>/dev/null; #https://unix.stackexchange.com/a/428508
 	pprint "perl" "$version"
 	
@@ -131,6 +140,7 @@ get_go() {
 	#then
 	#	printf "" # TODO fix error; must use pkg info
 	#fi	
+	printtitle "go"
 	pprint "go" "$version"
 
 	if command -v gccgo >/dev/null 2>&1; then
@@ -149,6 +159,8 @@ get_php() {
 	
 	# aliased main
 	version=$(php -v | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)')
+	
+	printtitle "php"
 	pprint "php" "$version"
 
 	for major in {4..10}; do
@@ -172,6 +184,7 @@ get_ruby() {
 	fi
 	
 	version=$(ruby --version | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)')
+	printtitle "ruby"
 	pprint "ruby" "$version"
 	
 	# TODO; pkg manager gem; to add or not to add 
@@ -194,6 +207,10 @@ get_c() {
 	for item in "gcc" "g++" "make" "clang" "cmake" "gmake"; do
 		if command -v $item >/dev/null 2>&1; then
 			version=$($item --version | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)' | head -n 1)
+			
+			if [ $count = 0 ]; then
+				printtitle "c/c++"
+			fi
 			pprint "$item" "$version"
 			count=$((count + 1))
 		fi
@@ -214,6 +231,10 @@ get_rust() {
 	for item in "rustc" "cargo"; do
 		if command -v $item >/dev/null 2>&1; then
 			version=$($item --version | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)' | head -n 1)
+			
+			if [ $count = 0 ]; then
+				printtitle "rust" 
+			fi
 			pprint "$item" "$version"
 			count=$((count + 1))
 		fi
@@ -228,10 +249,17 @@ get_rust() {
 }
 
 get_shells() {
+
+	count=0
 	for item in "bash" "zsh" "fish"; do
 		if command -v $item >/dev/null 2>&1; then
 			version=$($item --version | grep -E -o -m 1 '([0-9]+([.][0-9]+)+)')
+			
+			if [ $count = 0 ]; then
+				printtitle "shells" 
+			fi
 			pprint "$item" "$version"
+			count=$((count + 1))
 		fi
 	done
 	
